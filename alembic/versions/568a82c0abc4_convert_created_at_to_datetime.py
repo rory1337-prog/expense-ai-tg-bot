29f6ad755ea830187509b032d43a20cfa4017158
Rostyslav Ryzhkov <rostyslav.ryzhkov@icloud.com>
2026-07-11 11:24:43 +0200

    Fix PostgreSQL entry timestamp type

---
+++ alembic/versions/568a82c0abc4_convert_created_at_to_datetime.py
@@ -0,0 +1,40 @@
+"""convert created_at to datetime
+
+Revision ID: 568a82c0abc4
+Revises: ce011f122bb3
+Create Date: 2026-07-11 11:23:53.006365
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '568a82c0abc4'
+down_revision: Union[str, Sequence[str], None] = 'ce011f122bb3'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.alter_column(
+        "entries",
+        "created_at",
+        existing_type=sa.String(),
+        type_=sa.DateTime(),
+        postgresql_using="created_at::timestamp",
+        existing_nullable=False,
+    )
+
+
+def downgrade() -> None:
+    op.alter_column(
+        "entries",
+        "created_at",
+        existing_type=sa.DateTime(),
+        type_=sa.String(),
+        postgresql_using="created_at::text",
+        existing_nullable=False,
+    )
